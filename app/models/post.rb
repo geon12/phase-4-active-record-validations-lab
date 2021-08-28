7f28f37c6a7ca7c966f4c5b79f5361ccf47279b2
Cameron <camcreel@gmail.com>
2021-08-28 17:51:20 -0400

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ['Fiction','Non-Fiction']}
+
+    validate :title_must_be_clickbait
+
+    def title_must_be_clickbait
+        clickbait = ["Won't Believe","Secret","Top","Guess"]
+        if title
+            unless clickbait.any? {|word| title.include? word} 
+                errors.add(:title, "must be clickbait")
+            end
+        end
+    end
 end
